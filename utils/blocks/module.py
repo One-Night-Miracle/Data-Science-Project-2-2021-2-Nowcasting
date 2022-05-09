eb99ba915ec12355fc4145e2f68cdf30092d6796
shrria <58872002+shrria@users.noreply.github.com>
2022-05-10 04:28:56 +0700

    Create module.py

---
+++ utils/blocks/module.py
@@ -0,0 +1,33 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '')))
+from torch import nn
+import torch.nn.functional as F
+import torch
+from utils.utils import make_layers
+
+class EF(nn.Module):
+
+    def __init__(self, encoder, forecaster):
+        super().__init__()
+        self.encoder = encoder
+        self.forecaster = forecaster
+
+    def forward(self, input):
+        state = self.encoder(input)
+        output = self.forecaster(state)
+        return output
+
+class Predictor(nn.Module):
+    def __init__(self, params):
+        super().__init__()
+        self.model = make_layers(params)
+
+    def forward(self, input):
+        '''
+        input: S*B*1*H*W
+        :param input:
+        :return:
+        '''
+        input = input.squeeze(2).permute((1, 0, 2, 3))
+        output = self.model(input)
+        return output.unsqueeze(2).permute((1, 0, 2, 3, 4))
