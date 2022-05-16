3e8a1ac8673e1878a17d845eb278c42407cd7001
shrria <58872002+shrria@users.noreply.github.com>
2022-05-16 17:56:48 +0700

    Edited filepath & codeformat

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,4 @@
+from utils.blocks.activation import activation
 import os
 from utils.tools.ordered_easydict import OrderedEasyDict as edict
 import numpy as np
@@ -7,8 +8,9 @@ __C = edict()
 cfg = __C
 
 __C.GLOBAL = edict()
-__C.GLOBAL.DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-__C.GLOBAL.MAX_WORKERS = 12
+__C.GLOBAL.DEVICE = torch.device(
+    "cuda" if torch.cuda.is_available() else "cpu")
+__C.GLOBAL.MAX_WORKERS = 6
 __C.GLOBAL.BATCH_SIZE = 2
 __C.GLOBAL.ZR = edict()
 __C.GLOBAL.ZR.MAX_dBZ = 75.0
@@ -18,26 +20,28 @@ __C.GLOBAL.ZR.B = 1.6
 for dirs in ['/models/save']:
     if os.path.exists(dirs):
         __C.GLOBAL.MODEL_SAVE_DIR = dirs
-assert __C.GLOBAL.MODEL_SAVE_DIR is not None
+# assert __C.GLOBAL.MODEL_SAVE_DIR is not None
 
 __C.ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 __C.DATA_BASE_PATH = os.path.join(__C.ROOT_DIR, 'data')
 
-for dirs in ['/data/bkk_radar_images']:
+for dirs in [os.path.join(__C.DATA_BASE_PATH, 'bkk_radar_images')]:
     if os.path.exists(dirs):
         __C.RADAR_PNG_PATH = dirs
-for dirs in ['/data/bkk_radar_images']:
+for dirs in [os.path.join(__C.DATA_BASE_PATH, 'bkk_radar_images_dBZ')]:
     if os.path.exists(dirs):
         __C.PNG_PATH = dirs
-for dirs in ['/data/bkk_radar_images_dBZ']:
+for dirs in [os.path.join(__C.DATA_BASE_PATH, 'bkk_radar_images_dBZ')]:
     if os.path.exists(dirs):
         __C.MASK_PATH = dirs
 
 __C.EVALUATION = edict()
 # Image Cropping Region (TOP, LEFT, RIGHT, BOTTOM)
 __C.EVALUATION.CENTRAL_REGION = (0, 0, 2034, 2048)
-__C.EVALUATION.THRESHOLDS = np.array([0, 5.5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75])
-__C.EVALUATION.BALANCING_WEIGHTS = (1, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75)
+__C.EVALUATION.THRESHOLDS = np.array(
+    [0, 5.5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75])
+__C.EVALUATION.BALANCING_WEIGHTS = (
+    1, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75)
 
 __C.EVALUATION.VALID_DATA_USE_UP = True
 __C.EVALUATION.VALID_TIME = 20
@@ -48,7 +52,8 @@ __C.BENCHMARK.STAT_PATH = os.path.join(__C.DATA_BASE_PATH, 'benchmark_stat')
 if not os.path.exists(__C.BENCHMARK.STAT_PATH):
     os.makedirs(__C.BENCHMARK.STAT_PATH)
 
-__C.BENCHMARK.VISUALIZE_SEQ_NUM = 10  # Number of sequences that will be plotted and saved to the benchmark directory
+# Number of sequences that will be plotted and saved to the benchmark directory
+__C.BENCHMARK.VISUALIZE_SEQ_NUM = 10
 __C.BENCHMARK.IN_LEN = 5   # The maximum input frames
 __C.BENCHMARK.OUT_LEN = 20  # The maximum output frames
 __C.BENCHMARK.STRIDE = 5   # The stride
@@ -80,9 +85,10 @@ __C.ONM = edict()
 __C.ONM.ITERATOR = edict()
 __C.ONM.ITERATOR.WIDTH = 2034
 __C.ONM.ITERATOR.HEIGHT = 2048
-__C.ONM.ITERATOR.FILTER_RAINFALL = True           # Whether to discard part of the rainfall, has a denoising effect
-__C.ONM.ITERATOR.FILTER_RAINFALL_THRESHOLD = 0.28 # All the pixel values that are smaller than round(threshold * 255) will be discarded
+# Whether to discard part of the rainfall, has a denoising effect
+__C.ONM.ITERATOR.FILTER_RAINFALL = True
+# All the pixel values that are smaller than round(threshold * 255) will be discarded
+__C.ONM.ITERATOR.FILTER_RAINFALL_THRESHOLD = 0.28
 
 __C.MODEL = edict()
-from utils.blocks.activation import activation
 __C.MODEL.RNN_ACT_TYPE = activation('leaky', negative_slope=0.2, inplace=True)
